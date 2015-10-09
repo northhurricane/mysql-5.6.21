ea0c2fb6b7ff13d3092e38bee59478ca4cfda920
jiangyx <jiangyx@ctrip.com>
2015-10-09 14:28:30 +0800

    完成初步框架，可加载到mysql中

---
+++ storage/cfl/cfl_buffer.h
@@ -0,0 +1,15 @@
+#ifndef _CFL_BUFFER_H_
+#define _CFL_BUFFER_H_
+
+/*
+说明
+1、用于进行clf page的缓冲区，避免频繁分配释放
+2、可动态配置，增多或者减少缓冲区
+*/
+
+class Cfl_buffer
+{
+};
+
+
+#endif //_CFL_BUFFER_H_
---
+++ storage/cfl/cfl_page.h
@@ -0,0 +1,4 @@
+#ifndef _CFL_PAGE_H_
+#define _CFL_PAGE_H_
+
+#endif //_CFL_PAGE_H_
