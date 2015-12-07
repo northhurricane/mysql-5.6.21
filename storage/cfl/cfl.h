b231837dafc5b832fa79e4b2fbb27ffda4e67fc7
jiangyx <jiangyx@ctrip.com>
2015-12-07 17:57:31 +0800

    na

--- storage/cfl/cfl.h
+++ storage/cfl/cfl.h
@@ -6,5 +6,37 @@
 #include <field.h>
 #include <stdint.h>
 
+enum cfl_key_cmp {
+  KEY_INVALID,       /* invalid status */
+  KEY_EQUAL,         /* Find record equal to the key, else error */
+  KEY_GE,            /* Record greater or equal  */
+  KEY_LE,            /* Record less or equal */
+  KEY_G,             /* Record greater */
+  KEY_L,             /* Record less */
+};
+
+inline enum cfl_key_cmp
+my_key_func2clf_key_cmp(enum ha_rkey_function key_func)
+{
+  switch (key_func)
+  {
+  case HA_READ_KEY_EXACT:
+    return KEY_EQUAL;
+  case HA_READ_KEY_OR_NEXT:
+    return KEY_GE;
+  case HA_READ_KEY_OR_PREV:
+    return KEY_LE;
+  case HA_READ_AFTER_KEY:
+    return KEY_G;
+  case HA_READ_BEFORE_KEY:
+    return KEY_L;
+  default :
+    return KEY_INVALID;
+  }
+  return KEY_INVALID;
+}
+
+#define CFL_LOCATE_PAGE_NULL (0xFFFFFFFF)
+
 #endif //_CFL_H_
 
