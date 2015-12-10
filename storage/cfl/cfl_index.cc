808d0b0fedc0b638dbd120267afe516116258b4b
jiangyx <jiangyx@ctrip.com>
2015-12-10 14:26:31 +0800

    完善页面定位的算法和说明

--- storage/cfl/cfl_index.cc
+++ storage/cfl/cfl_index.cc
@@ -101,6 +101,29 @@ CflIndex::Destroy(CflIndex *index)
   return 0;
 }
 
+/*
+页面定位说明。
+情况EQUAL:
+  定位的记录是最前一个等于搜索key的记录。
+  定位页面的key等于或者大于搜索key，定位页面的前一个页面的key必定小于搜索key。
+  如果定位到的页面是虚拟最大页面，则说明没有满足条件的记录
+情况GE:
+  定位的记录是情况E的记录和气候的记录。
+  定位页面和情况EQUAL相同
+  如果定位到的页面是虚拟最大页面，则说明没有满足条件的记录
+情况G:
+  定位的记录是大于搜索key的最小记录。
+  定位页面的key大于搜索key，定位页面的前一个页面的key必定等于或者小于搜索key
+  如果定位到的页面是虚拟最大页面，则说明没有满足条件的记录
+情况LE:
+  定位的记录是最后一个等于搜索key的记录
+  定位页面的key大于搜索key，定位页面的前一个页面的key必定等于或者小于搜索key
+  在此处，不存在没有满足条件的记录
+情况L:
+  定位的记录是小于搜索key的最大记录
+  定位页面的key等于或者大于搜索key，定位页面的前一个页面的key必定小于搜索key
+  在此处，不存在没有满足条件的记录
+*/
 uint32_t
 CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
 {
@@ -117,7 +140,7 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
   low = 0;//虚拟最小记录
   high = index_node_count + 1; //虚拟最大记录
   mid = (low + high) / 2; //中间记录，
-  //
+  //定位
   while (low + 1 < high)
   {
     index_key = ReadNthIndexNode(mid - 1);
@@ -139,8 +162,9 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
   uint32_t page_no = CFL_LOCATE_PAGE_NULL; //1-based
   if (found)
   {
-    //找到和key相等的页面索引
-    //根据key_cmp确定页面
+    //1、找到和key相等的页面索引
+    //2、当定位到某个页面的key和搜索key相等，则EQUAL/GE情况必然存在满足条件的
+    //记录。只有G情况存在不满足条件的记录
     page_no = mid;
     switch(key_cmp)
     {
@@ -173,9 +197,8 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
       //定位第一个大于key的page
       //原因1，对于G来说，所有页面的key值等于参数key值得页面必然是不符合要求，
       //也就是，这些页面的时间标记都小于参数key，所以必须是大于该key的
-      //原因2，对于LE来说，由于时间字段不是唯一标识
-      //所以，最后一个页面的key值等于参数key的，在其后页面的记录，
-      //有可能等于参数key
+      //原因2，对于LE来说，由于时间字段不是唯一标识。所以，最后一个页面的key值
+      //等于搜索key的，在其后页面的记录，有可能等于搜索key
       page_no++;
       while (page_no < (index_node_count + 1))
       {
@@ -186,11 +209,20 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
         }
         page_no++;
       }
-      if (index_key == key)
+      if (page_no == (index_node_count + 1))
       {
-        //未找到相应的key
-        DBUG_ASSERT(page_no == (index_node_count + 1));
-        page_no = CFL_LOCATE_PAGE_NULL;
+        //只有虚拟最大页面的key大于搜索key
+        if (key_cmp == KEY_G)
+        {
+          //所有记录都不满足条件
+          page_no = CFL_LOCATE_PAGE_NULL;
+        }
+        else
+        {
+          //所有记录都满足条件
+          DBUG_ASSERT(key_cmp == KEY_LE);
+          page_no--;
+        }
       }
       break;
     }
@@ -202,15 +234,14 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
   }
 
   //未找到和key相等的页面索引，此时key的值在low和high之间
-  //也就是参数key的值必然落在high所指向的page页内
+  //也就是参数key的值必然大于low所指页面的记录
   switch(key_cmp)
   {
   case KEY_EQUAL:
   case KEY_GE:
   case KEY_G:
   {
-    //所需要定位的记录在high所指向的page
-    //如果high等于index_node_count + 1则说明没有合适的page页
+    //如果high等于index_node_count + 1则说明最大页的所有记录都小于搜索key
     if (high == (index_node_count + 1))
       page_no = CFL_LOCATE_PAGE_NULL;
     else
@@ -223,7 +254,7 @@ CflIndex::LocatePage(cfl_dti_t key, cfl_key_cmp key_cmp)
     //所有小于high的页面都是满足条件的页面，high所指向的页面可能部分满足条件
     //如果high等于index_node_count + 1则所定位的记录在low的page中
     if (high == (index_node_count + 1))
-      page_no = high - 1;
+      page_no = index_node_count;
     else
       page_no = high;
     break;
