6599a06a9e1d93d88df84d90e98a65548039adeb
jiangyx <jiangyx@ctrip.com>
2016-03-15 14:35:19 +0800

    完成定位搜索的改造。并进行delete和select的改造

--- storage/cfl/ha_cfl.cc
+++ storage/cfl/ha_cfl.cc
@@ -92,6 +92,57 @@ cfl_isearch_init(cfl_isearch_t &isearch)
   isearch.key_cmp = KEY_EQUAL;
 }
 
+inline bool isearch_key_match(const cfl_isearch_t &isearch, cfl_dti_t rowkey)
+{
+  //比较该记录的key，是否符合isearch中比较的。如果不符合，则说明所有记录已经
+  //完成获取。将cursor的position设置为CFL_CURSOR_AFTER_END
+  bool matched;
+  switch (isearch.key_cmp)
+  {
+  case KEY_EQUAL:
+    if (rowkey != isearch.key)
+      matched = false;
+    else
+      matched = true;
+    break;
+  case KEY_GE:
+  case KEY_G:
+    matched = true;
+    break;
+  case KEY_LE:
+    if (rowkey <= isearch.key)
+      matched = true;
+    else
+      matched = false;
+    break;
+  case KEY_L:
+    if (rowkey < isearch.key)
+      matched = true;
+    else
+      matched = false;
+    break;
+  default:
+    DBUG_ASSERT(false);
+    matched = false;
+  }
+  return matched;
+}
+
+inline void
+cfl_cursor_fill_row(cfl_cursor_t &cursor)
+{
+  uint8_t *row;
+  void *page_data;
+  uint32_t row_length;
+
+  DBUG_ASSERT(cursor.page != NULL);
+  page_data = cursor.page->page();
+  row = cfl_page_nth_row((uint8_t*)page_data, cursor.row_no);
+  row_length = cfl_page_nth_row_length((uint8_t*)page_data, cursor.row_no);
+  cfl_cursor_row_set(cursor, row);
+  cfl_cursor_row_length_set(cursor, row_length);
+}
+
 static int
 cfl_check_create_info(TABLE *table_arg, HA_CREATE_INFO *create_info)
 {
@@ -634,7 +685,7 @@ int ha_cfl::delete_rows(Field ** fields, cfl_dti_t key
   }
 
   bool over;
-  rc = locate_next(over);
+  rc = locate_next2(over);
   if (over)
   {
     return 0;
@@ -683,7 +734,7 @@ int ha_cfl::delete_rows(Field ** fields, cfl_dti_t key
       break;
     }
     //获取下一条记录
-    rc = locate_next(over);
+    rc = locate_next2(over);
     if (over)
       break;
   } while (true);
@@ -1541,7 +1592,7 @@ ha_cfl::index_next(uchar *buf)
   int rc = 0;
   bool over = true;
 
-  rc = locate_next(over);
+  rc = locate_next2(over);
   if (!rc)
   {
     if (over)
@@ -1670,6 +1721,11 @@ int ha_cfl::locate_cursor()
   return rc;
 }
 
+/*
+  2016-3-14拆分locate_next函数。抽取出获取物理记录的部分代码为底层函数。
+  重新构造locate_next函数
+*/
+//#ifdef _USE_IT
 int
 ha_cfl::locate_next(bool &over)
 {
@@ -1818,6 +1874,174 @@ index_next_over :
   over = true;
 
   return 0;
+}
+//#endif //_USE_IT
+
+//#ifdef _USE_IT
+int
+ha_cfl::locate_next2(bool &over)
+{
+  uint32_t page_no_orig;  //当前的页号
+  uint32_t row_no_orig;   //当前的页号
+  int rc = 0;
+
+  page_no_orig = cfl_cursor_page_no_get(cursor_);
+  row_no_orig = cfl_cursor_row_no_get(cursor_);
+
+  /*
+    locate next必须是locate_cursor函数定位后，才能被调用
+    分为3种情况
+    1、还未进行next操作，此时cursor的postion为CFL_CURSOR_BEFOR_START。
+       由于在locate_cursor中定位到第一条数据，此时只需要根据cursor中的信息，
+       进行数据获取即可。
+    2、已经获取完最后一条数据，此时postion为CFL_CURSOR_AFTER_END。
+       直接返回表明结果获取结束。
+    3、获取下一条记录。此时cursor的postion记录的是上一条被获取的记录
+  */
+  //根据postion进行next的初始化
+  over = false;
+  if (cfl_cursor_position_get(cursor_) == CFL_CURSOR_BEFOR_START)
+  {
+    /*情况1处理*/
+    DBUG_ASSERT(cfl_cursor_page_get(cursor_) != NULL);
+    cfl_cursor_position_set(cursor_, 1);
+    cfl_cursor_fill_row(cursor_);
+    return 0;
+  }
+  else if (cfl_cursor_position_get(cursor_) == CFL_CURSOR_AFTER_END)
+  {
+    /*情况2处理*/
+    over = true;
+    return 0;
+  }
+  else
+  {
+    /*情况3处理*/
+    //获取下一条记录的位置信息
+  }
+
+  do
+  {
+    //获取下一条记录
+    rc = next_physical_row(cfl_table_, cursor_, over);
+    if (!rc)
+    {
+      //错误处理
+    }
+    if (over)
+    {
+      //不存在locate的下一条记录
+      break;
+    }
+    bool renext = false;
+    renext = check_renext();
+    if (renext)
+      continue;
+    bool matched = false;
+    matched = locate_key_match();
+    if (matched)
+    {
+      over = false;
+      break;
+    }
+  }
+  while (true);
+
+  if (over)
+  {
+    //释放资源，设置获取前的页面和行信息
+    CflPage *page;
+    page = cfl_cursor_page_get(cursor_);
+    if (page != NULL)
+      CflPageManager::PutPage(page);
+    cfl_cursor_page_set(cursor_, NULL);
+    cfl_cursor_page_no_set(cursor_, page_no_orig);
+    cfl_cursor_row_no_set(cursor_, row_no_orig);
+    cfl_cursor_position_set(cursor_, CFL_CURSOR_AFTER_END);
+  }
+  else
+  {
+    //更新position信息
+    uint64_t position = 0;
+    position = cfl_cursor_position_get(cursor_);
+    position++;
+    cfl_cursor_position_set(cursor_, position);
+  }
+
+  return 0;
+}
+//#endif //_USE_IT
+
+int ha_cfl::next_physical_row(CflTable *table, cfl_cursor_t &cursor
+                              , bool &over)
+{
+  uint32_t page_no;  //进行定位的页号
+  uint32_t row_no;   //进行定位的行号
+  CflPage *page = NULL;
+
+  //获取下一条记录
+  /*
+    有如下情况
+    1、下一条记录在当前页
+    2、下一条记录在下一页。此时又存在两种情况
+      2-1、下一页存在。如果记录页存在，其中必有记录，next一定会获得一条记录
+      2-2、下一页不存在。说明已经完成所有记录的next
+  */
+  page_no = cfl_cursor_page_no_get(cursor);
+  row_no = cfl_cursor_row_no_get(cursor);
+
+  page = cfl_cursor_page_get(cursor);
+  DBUG_ASSERT(page != NULL);
+  void *page_data = page->page();
+  uint32_t row_count = cfl_page_read_row_count(page_data);
+  over = false;
+  //假设当前页存在下一条记录。情况1
+  row_no++;
+  //判断情况1是否成立
+  if (row_no >= row_count)
+  {
+    DBUG_ASSERT(row_no == row_count);//理论上不存在大于情况
+    //当前页不存在下一条记录。情况2
+    //行不在当前页，释放cursor当前的页，取得下一页
+    CflPageManager::PutPage(page);
+    page_no++;
+    row_no = 0;
+    page = CflPageManager::GetPage(table->GetStorage(), page_no);
+    //后续页是否存在
+    if (page == NULL)
+    {
+      //情况2-2
+      over = true;
+    }
+  }
+
+  if (!over)
+  {
+    //page_no, row_no, page这三个，确定行
+    cfl_cursor_page_set(cursor, page);
+    cfl_cursor_page_no_set(cursor, page_no);
+    cfl_cursor_row_no_set(cursor, row_no);
+    //获取其他相关信息
+    uint8_t *row;
+    void *page_data;
+    uint32_t row_length;
+
+    page_data = page->page();
+    row = cfl_page_nth_row((uint8_t*)page_data, row_no);
+    row_length = cfl_page_nth_row_length((uint8_t*)page_data, row_no);
+    cfl_cursor_row_set(cursor, row);
+    cfl_cursor_row_length_set(cursor, row_length);
+  }
+
+  return 0;
+}
+
+bool ha_cfl::locate_key_match()
+{
+  cfl_dti_t rowkey;
+  uint8_t *row = cfl_cursor_row_get(cursor_);
+  rowkey = cfl_row_get_key_data(table->field, row);
+  return isearch_key_match(isearch_, rowkey);
 }
 
 void
